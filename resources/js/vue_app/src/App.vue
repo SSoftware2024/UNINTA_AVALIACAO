e233c2c5900a566264ff96fd41b7521a38fb363b
Soluções Software <ssolucoessoftware@gmail.com>
2025-12-16 17:51:44 -0300

    teste pinia

--- resources/js/vue_app/src/App.vue
+++ resources/js/vue_app/src/App.vue
@@ -1,9 +1,12 @@
 <script setup>
+import { useCounterStore } from './stores/counter'
+const counter = useCounterStore()
 
 </script>
 
 <template>
-  <h1 class="text-primary">Olá bootstrap!</h1>
+  <h1 class="text-primary">Olá bootstrap! Pinia({{ counter.name }} - {{ counter.doubleCount }}): {{ counter.count }}</h1>
+  <h2 @click="counter.increment()">Click</h2>
 </template>
 
 
