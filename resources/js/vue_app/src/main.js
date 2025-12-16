961f6611505543d1fccee318ebdc2b7b8a963544
Soluções Software <ssolucoessoftware@gmail.com>
2025-12-16 17:37:59 -0300

    bootstrap instalado

--- resources/js/vue_app/src/main.js
+++ resources/js/vue_app/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./style.css";
 import App from "./App.vue";
 
